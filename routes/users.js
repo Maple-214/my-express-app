391bf984b7b6224b38b2d26b84e3f063604ed232
maple <290204957@qq.com>
2023-06-24 22:38:40 +0900

    feat: homePageContent

--- routes/users.js
+++ routes/users.js
@@ -109,9 +109,62 @@ const data = [
   }
 ]
 
+const data2 = {
+  slides: [
+    { id: 0, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBOBdFe_MJJj7v0fJr5tBiGOS64FfmrMkyEQ&usqp=CAU" },
+    { id: 1, image: "https://image.maigoo.com/upload/images/20221101/1404314044_750x498.jpg" },
+    { id: 2, image: "https://storage.googleapis.com/pos.shopline.hk/2021/11/onlineshop-vs-marketplace-hero-image.jpg" },
+  ],
+  category: [
+    { id: 0, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/187640/12/30456/5256/639c2315Ebc95c142/350a8f0c766f5460.png", title: "京东超市" },
+    { id: 1, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/178015/31/13828/6862/60ec0c04Ee2fd63ac/ccf74d805a059a44.png", title: "数码电器" },
+    { id: 2, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/54043/33/19389/4660/62b049dbE3b9aef75/2fcd31afd5d702e4.png", title: "京东新百货" },
+    { id: 3, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/177902/16/13776/5658/60ec0e71E801087f2/a0d5a68bf1461e6d.png", title: "京东生鲜" },
+    { id: 4, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/196472/7/12807/7127/60ec0ea3Efe11835b/37c65625d94cae75.png", title: "京东到家" },
+    { id: 5, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/185733/21/13527/6648/60ec0f31E0fea3e0a/d86d463521140bb6.png", title: "充值缴费" },
+    { id: 6, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/34248/39/16616/4689/62bbbdccE9f11132e/d51caf15f2f412b2.png", title: "附近好店" },
+    { id: 7, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/37709/6/15279/6118/60ec1046E4b5592c6/a7d6b66354efb141.png", title: "PLUS会员" },
+    { id: 8, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/191060/24/12861/6818/60ec11f2E67cf5ee6/c264378678b3cd96.png", title: "京东国际" },
+    { id: 9, icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/186882/8/12149/5894/60ec1250E9335241a/b22054613aa8ae75.png", title: "京东拍卖" },
+  ],
+  shopInfo: "https://www.chuangkit.com/shejixuexi/images/banner2.jpg",
+  recommend: [
+    {
+      'image': 'https://www.imuraya.co.jp/media_images/2019/03/box-azukiba-maccha.jpg',
+      'productName': '商品名1',
+      'price': '20.0',
+      'mallPrice': '30.0',
+    },
+    {
+      'image': 'https://www.calbee.co.jp/common/utility/binout.php?db=products&f=3152',
+      'productName': '商品名2',
+      'price': '30.0',
+      'mallPrice': '35.0',
+    },
+    {
+      'image': 'https://www.asahi-gf.co.jp/products/images/baby/milk/wakodo/gungun/40195890.jpg',
+      'productName': '商品名3',
+      'price': '40.0',
+      'mallPrice': '48.0',
+    },
+    {
+      'image': 'https://item-shopping.c.yimg.jp/i/l/pc-parts_tkm004',
+      'productName': '商品名4',
+      'price': '60.0',
+      'mallPrice': '78.0',
+    },
+  ],
+  advertesPicture:"https://pic40.photophoto.cn/20160910/0017029583521992_b.jpg"
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/goods', function (req, res, next) {
   res.send(data);
 });
+
+router.get('/homePageContent', function (req, res, next) {
+  res.send(data2);
+});
+
 
 module.exports = router;
