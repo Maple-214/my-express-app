652c921eaa0820c0089a09b3ed54f76d582494cd
maple <290204957@qq.com>
2023-06-24 22:44:35 +0900

    fix:path

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', usersRouter);
+app.use('/v2/api', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
